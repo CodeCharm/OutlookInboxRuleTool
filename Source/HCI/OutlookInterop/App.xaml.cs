30ef92fb862565bd2e7a1ec778a899f8d994a95a
Alan McBee <com.github@codecharm.com>
2022-09-28 13:32:48 -0700

    Initial library for Human-Computer Interface (HCI) or UI (#1)
    Make sure workstation setup is good
    Make sure PR policy is good
    
    Co-authored-by: Alan McBee <alan.mcbee@microsoft.com>

---
+++ Source/HCI/OutlookInterop/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OutlookInterop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Source/HCI/OutlookInterop/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OutlookInterop
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

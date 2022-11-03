a7cb03c07e18f11ecc6b8eaa37232d69cec04608
Alan McBee <alan.mcbee@microsoft.com>
2022-11-03 11:30:02 -0700

    remove Android, iOS, Mac targets

--- Source/HCI/OutlookInboxRuleApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace CodeCharm.OutlookInboxRuleApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}

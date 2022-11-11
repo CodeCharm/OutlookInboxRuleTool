bde57ab4c91cc7226d811db8b742489ee004166c
Alan McBee <alan.mcbee@microsoft.com>
2022-11-10 17:13:30 -0800

    Going with WPF, but heavy use of MVVM to simplify migration later

--- Source/HCI/OutlookInboxRuleApp/App.xaml.cs
+++ Source/HCI/OutlookInboxRuleApp/App.xaml.cs
@@ -1,12 +1,17 @@
-﻿namespace OutlookInboxRuleApp
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OutlookInboxRuleApp
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            InitializeComponent();
-
-            MainPage = new AppShell();
-        }
     }
 }
